b24bf21731863ab07fa0c04b8e6203015d2ced45
AyaAid <aya.lakhel11@gmail.com>
2025-11-13 11:27:27 +0100

    fix deployement

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './i18n.js'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Analytics from './components/Analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights />
     <Analytics />
     <App />
   </React.StrictMode>
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = ProcessingInstruction.env.SUPABASE_URL;
-const supabaseKey = ProcessingInstruction.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

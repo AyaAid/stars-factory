461e6dcaa778eb927c80c3f0e1fa2517d52a61d4
AyaAid <aya.lakhel11@gmail.com>
2025-11-13 10:43:08 +0100

    security

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://iapbjaurdvppkmtgbypy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcGJqYXVyZHZwcGttdGdieXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5MDk1OTQsImV4cCI6MjA2NDQ4NTU5NH0.hGYtIO6hMSaM0FxiPq8RuAzqBFZ-32KYaj3s73sexUw";
+const supabaseUrl = ProcessingInstruction.env.SUPABASE_URL;
+const supabaseKey = ProcessingInstruction.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
